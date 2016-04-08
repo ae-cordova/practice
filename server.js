2fbdc32f1860d39b60c1fbf1cc05a7cc68992293
Angel Cordova <aecord00@gmail.com>
2016-04-08 13:13:19 -0500

    Ya enconté el error del que les hablaba!!! había un caracter raro al inicio del archivo html que el sublime pone por default, ya lo habia visto en practicas anteriores pero no me habia hecho ruido, hasta ahora. pues resulta que el readFileSync devuelve o un string o un objeto, en este caso esperaba que sirviera la pagina webestatica que habiamos creado en el archivo public pero no, por culpa de ese caracter (maldito <) convertia todo el contenido del archivo en string y eso es lo que mostraba en el navegador, el codigo no la pagina... :) ya encontree el erroooor!!!

--- server.js
+++ server.js
@@ -3,7 +3,7 @@
 //Requiriendo las librerias necesarias:
 const http = require('http');
 const fs = require ("fs"); //filesystem
-
+//const path = require ('')
 
 //Definiendo el puerto de escucha, o bien si no esta especificado se pone por default el 8080
 const port = process.env.PORT||8080;
@@ -17,12 +17,13 @@ server.listen(port);//empezamos a excuchar por el pueto definido
 
 
 function onRequest (req, res){
-	fs.readFile('public/index.html', function(err,file){
+	let file = fs.readFileSync('public/index.html');/*, function(err,file){
 		if(err){
 			return res.end(err.message)
 		}
 		res.end(file);
-	});
+	});*/
+	res.end(file);
 }
 
 function onListening(){
