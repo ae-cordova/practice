a9a1e061ce36f811ba1ab1ac4af4a80d975e54d9
Angel Cordova <aecord00@gmail.com>
2016-04-08 11:46:55 -0500

    Modificamos el server para que se maneje a traves de event emitter en lugar de callbacks, tambien agregamos una carpeta llamada public enla que pusimos una pagina estatica para que sea llamda desde el evento onRequest. el tema es que no se por que no logro que salga el contenido de la pagina sino el codigo html en el navegador!!! esta raro por que cuando lo abro directamente con firefox si lo interpreta... voy a tener que googlear..XD...

--- server.js
+++ server.js
@@ -1,10 +1,30 @@
 'use strict'
 
+//Requiriendo las librerias necesarias:
 const http = require('http');
+const fs = require ("fs"); //filesystem
+
+
+//Definiendo el puerto de escucha, o bien si no esta especificado se pone por default el 8080
 const port = process.env.PORT||8080;
 
-const server= http.createServer();
+const server= http.createServer();//Creamos el server
+
+server.on('request', onRequest);//Definimos que hacer en el evento request
+server.on('listening', onListening);//Definimos que hacer al inicial la escucha del server
+
+server.listen(port);//empezamos a excuchar por el pueto definido
 
 
-server.listen(port);
+function onRequest (req, res){
+	fs.readFile('public/index.html', function(err,file){
+		if(err){
+			return res.end(err.message)
+		}
+		res.end(file);
+	});
+}
 
+function onListening(){
+	console.log("Server Up and Running on port "+ port);
+}
