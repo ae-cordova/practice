80b913243b83d0326014397972f151491a4000ec
Angel Cordova <aecord00@gmail.com>
2016-04-08 19:28:46 -0500

    Agregamos la manipulacion de archivos con la libreria path y cambiamos el readfile por el asincrono...

--- server.js
+++ server.js
@@ -3,7 +3,7 @@
 //Requiriendo las librerias necesarias:
 const http = require('http');
 const fs = require ("fs"); //filesystem
-//const path = require ('')
+const path = require ('path');
 
 //Definiendo el puerto de escucha, o bien si no esta especificado se pone por default el 8080
 const port = process.env.PORT||8080;
@@ -17,13 +17,13 @@ server.listen(port);//empezamos a excuchar por el pueto definido
 
 
 function onRequest (req, res){
-	let file = fs.readFileSync('public/index.html');/*, function(err,file){
+	let fileName = path.join(__dirname,'public','index.html');
+	let file = fs.readFile(fileName, function(err,file){
 		if(err){
-			return res.end(err.message)
+			return res.end(err.message);
 		}
 		res.end(file);
-	});*/
-	res.end(file);
+	});
 }
 
 function onListening(){
